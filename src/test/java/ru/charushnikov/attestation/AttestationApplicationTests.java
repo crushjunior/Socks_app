2574c2e3f77e69b88b15b93529aaa20703347b5d
crushjunior <charushnikovvadim@gmail.con>
2023-08-05 09:20:27 +0300

    Initial commit

---
+++ src/test/java/ru/charushnikov/attestation/AttestationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.charushnikov.attestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttestationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
